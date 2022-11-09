626362bb3eb96ada5632dac5241bf0113e062cee
Pasquale <pasquale.coppola6@gmail.com>
2022-11-09 17:44:49 -0500

    updates

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,18 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
 
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'carts';
-    products: any[] = [
+    products: IProduct[] = [
      
             {
                 "productId": 2,
@@ -40,4 +42,7 @@ export class ProductListComponent {
         this.showImage = !this.showImage;
     }
 
+    ngOnInit() {
+        console.log('in OnInit');
+    }
 }
