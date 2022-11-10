0d460fae7803b7b68c8fa291a8c887cffd724dc4
Pasquale <pasquale.coppola6@gmail.com>
2022-11-10 11:48:10 -0500

    events

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,10 +13,20 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'carts';
+    
+    private _listFilter: string = '';
+    get listFilter(): string {
+        return this._listFilter;
+    }
+        set listFilter(value: string) {
+            this._listFilter = value;
+            console.log('In Setter:', value);
+            this.filteredProducts = this.performFilter(value);
+        }
+
+filteredProducts: IProduct[] = [];
     products: IProduct[] = [
-     
-            {
+             {
                 "productId": 2,
                 "productName": "Garden Cart",
                 "productCode": "GDN-0023",
@@ -38,11 +48,22 @@ export class ProductListComponent implements OnInit {
               }
     ];
 
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().includes(filterBy));
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit() {
-        console.log('in OnInit');
+        this.listFilter = 'cart';
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
 }
